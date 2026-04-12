a68e2b30f3b306478241782f90fc1ead69f6aa97
Youness Rziqi <younessrziki204@gmail.com>
2026-04-12 07:00:31 +0100

    Update

--- C_Lang_Deep_Dive/Big_Projects/Codexion/codexion.h
+++ C_Lang_Deep_Dive/Big_Projects/Codexion/codexion.h
@@ -1,51 +1,51 @@
-// #ifndef CODEXION_H
-// #define CODEXION_H
+#ifndef CODEXION_H
+#define CODEXION_H
 
-// #include <stdio.h>
-// #include <pthread.h>
-// #include <string.h>
-// #include <sys/time.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <sys/time.h>
 
 
-// typedef struct s_args
-// {
-//     int number_of_coders;
-//     long long time_to_burnout;
-//     long long time_to_compile;
-//     long long time_to_debug;
-//     long long time_to_refactor;
-//     int number_of_compiles_required;
-//     long long dongle_cooldown;
-//     char *scheduler;
-// } t_args;
+typedef struct s_args
+{
+    int number_of_coders;
+    long long time_to_burnout;
+    long long time_to_compile;
+    long long time_to_debug;
+    long long time_to_refactor;
+    int number_of_compiles_required;
+    long long dongle_cooldown;
+    char *scheduler;
+} t_args;
 
-// typedef enum e_coder_state
-// {
-//     COMPILING,
-//     DEBUGGING,
-//     REFACTORING,
-//     WAITING
+typedef enum e_coder_state
+{
+    COMPILING,
+    DEBUGGING,
+    REFACTORING,
+    WAITING
 
-// } t_coder_state;
+} t_coder_state;
 
-// typedef struct s_coder
-// {
+typedef struct s_coder
+{
 
-//     int coder_number;
-//     long deadlline;
-//     long last_compile_time;
-//     struct s_coder *left;  
-//     struct s_coder *right;
-//     t_coder_state status;
+    int coder_number;
+    long deadlline;
+    long last_compile_time;
+    struct s_coder *left;  
+    struct s_coder *right;
+    t_coder_state status;
 
-// } t_coder;
+} t_coder;
 
-// // short           is_full_digit(char *s);
-// // long long       ft_atoi(const char *s);
-// // t_args          convert_args(int argc, char **argv);
-// // short           is_empty_args(t_args args);
+// short           is_full_digit(char *s);
+// long long       ft_atoi(const char *s);
+// t_args          convert_args(int argc, char **argv);
+// short           is_empty_args(t_args args);
 
-// #include "utils_zero.h"
-// #include "pars_args.h"
+#include "utils_zero.h"
+#include "pars_args.h"
 
-// #endif
+#endif
