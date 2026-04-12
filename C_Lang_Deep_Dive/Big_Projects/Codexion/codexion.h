94569d4ea76e7a0eae859dd2c4cfc877c7031ca9
Youness Rziqi <younessrziki204@gmail.com>
2026-04-12 12:30:12 +0100

    Update

--- C_Lang_Deep_Dive/Big_Projects/Codexion/codexion.h
+++ C_Lang_Deep_Dive/Big_Projects/Codexion/codexion.h
@@ -1,102 +1,78 @@
 #ifndef CODEXION_H
-# define CODEXION_H
+#define CODEXION_H
 
-# include <pthread.h>
-# include <sys/time.h>
-# include <stddef.h>
-# include <string.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+// #include <string.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
-# define CDX_FIFO 0
-# define CDX_EDF  1
-# define MAX_CODERS 250
 
 typedef struct s_args
 {
-	int			num_coders;
-	long long	ttb;
-	long long	ttc;
-	long long	ttd;
-	long long	ttr;
-	int			compiles_req;
-	long long	cooldown;
-	int			scheduler;
-}	t_args;
+    int number_of_coders;
+    long long time_to_burnout;
+    long long time_to_compile;
+    long long time_to_debug;
+    long long time_to_refactor;
+    int number_of_compiles_required;
+    long long dongle_cooldown;
+    char *scheduler;
+} t_args;
 
-typedef struct s_request
+typedef enum e_coder_state
 {
-	int			coder_id;
-	long long	sort_key;
-}	t_request;
+    COMPILING,
+    DEBUGGING,
+    REFACTORING,
+    WAITING
 
-typedef struct s_pqueue
-{
-	t_request	data[MAX_CODERS];
-	int			size;
-}	t_pqueue;
+} t_coder_state;
 
 typedef struct s_dongle
 {
-	int				id;
-	pthread_mutex_t	mutex;
-	pthread_cond_t	cond;
-	int				is_held;
-	long long		available_at;
-	t_pqueue		queue;
-}	t_dongle;
+    short               is_available;
+    struct s_dongle     *left_dongle;
+    struct s_dongle     *right_dongle;
 
-typedef struct s_shared	t_shared;
+} t_dongle;
+
+typedef struct s_order_list
+{
+    int                 order;
+    struct s_order_list *next;
+} t_order_list;
 
 typedef struct s_coder
 {
-	int			number;
-	int			compile_count;
-	long long	last_compile_start;
-	long long	deadline;
-	int			left_id;
-	int			right_id;
-	int			first_id;
-	int			second_id;
-	pthread_t	thread;
-	t_shared	*shared;
-}	t_coder;
+    int             coder_number;
+    long long       last_compile_time;  // Last time compilation started
+    long long       deadline;           // Burnout deadline (last_compile_start + time_to_burnout)
+    long long       creation_time;      // Time when coder thread was created
+    t_dongle        dongle;  
+    t_coder_state   status;
+    short           is_usb_avilable;
 
-struct s_shared
+} t_coder;
+
+typedef struct s_simulation
 {
-	t_args			args;
-	t_dongle		*dongles;
-	t_coder			*coders;
-	int				running;
-	long long		start_time;
-	pthread_mutex_t	log_mutex;
-	pthread_mutex_t	state_mutex;
-	int				finished_count;
-	pthread_t		monitor_thread;
-};
+    t_args          *data;
+    t_coder         coder;
+    t_order_list    *order_list;
+    pthread_mutex_t *mutex_lock;
+    
+} t_simulation;
 
-t_args		convert_args(int argc, char **argv);
-int			is_empty_args(t_args args);
-int			is_full_digit(char *s);
-long long	ft_atoi(const char *s);
-long long	get_time_ms(void);
-long long	get_timestamp(t_shared *shared);
-void		log_message(t_shared *shared, int num, const char *msg);
-void		precise_sleep(long long ms, t_shared *shared);
-void		ft_putnbr_buf(long long n, char *buf, int *pos);
-int			init_simulation(t_shared *shared, t_args *args);
-void		cleanup_simulation(t_shared *shared);
-void		pq_init(t_pqueue *pq);
-void		pq_push(t_pqueue *pq, t_request req);
-t_request	pq_pop(t_pqueue *pq);
-int			pq_is_empty(t_pqueue *pq);
-int			get_left_dongle_id(t_coder *coder);
-int			get_right_dongle_id(t_coder *coder, int total);
-void		acquire_dongle(t_dongle *d, t_coder *c, t_shared *s);
-void		release_dongle(t_dongle *d, t_shared *s);
-void		*run_simulation(void *arg);
-void		*monitor_routine(void *arg);
-int			is_sim_running(t_shared *shared);
-void		do_compile(t_coder *c, t_shared *s);
-void		do_debug(t_coder *c, t_shared *s);
-void		do_refactor(t_coder *c, t_shared *s);
+// short           is_full_digit(char *s);
+// long long       ft_atoi(const char *s);
+// t_args          convert_args(int argc, char **argv);
+// short           is_empty_args(t_args args);
+
+#include "utils_zero.h"
+#include "pars_args.h"
+#include "threads.h"
+#include "errors.h"
 
 #endif
--- C_Lang_Deep_Dive/Big_Projects/Codexion/main.c
+++ C_Lang_Deep_Dive/Big_Projects/Codexion/main.c
@@ -3,48 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yrziqi <yrziqi@student.42.fr>               +#+  +:+       +#+        */
+/*   By: yrziqi <yrziqi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/12 07:25:48 by yrziqi             #+#    #+#             */
-/*   Updated: 2026/04/12 08:30:00 by yrziqi            ###   ########.fr       */
+/*   Created: 2026/04/12 07:25:48 by yrziqi            #+#    #+#             */
+/*   Updated: 2026/04/12 12:04:18 by yrziqi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
-#include <stdlib.h>
-#include <unistd.h>
-
-// Global simulation context
-t_simulation_context *g_sim_ctx = NULL;
 
 short simulation(t_args data)
 {
-    t_args *args = malloc(sizeof(t_args));
-    if (!args) return null_error_message();
-    *args = data;
-    
-    g_sim_ctx = create_simulation(args);
-    if (!g_sim_ctx) {
-        free(args);
-        return null_error_message();
-    }
-    
-    // TODO: Start monitor thread and wait for simulation to complete
-    // For now, sleep briefly then cleanup
-    sleep(1);
-    
-    destroy_simulation(g_sim_ctx);
+    pthread_t **p_th;
+	t_simulation sim_data;
+
+	p_th = NULL;
+	sim_data.data = &data;
+	
+	
+	sim_data.order_list = NULL;
+    pthread_mutex_init(&sim_data.mutex_lock, NULL);
+
+    p_th = creat_n_threads_and_start_sim(data.number_of_coders, &sim_data);
+
+    if (!p_th)
+        return (null_error_message());
+
     return 0;
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_args data;
-    
-    data = convert_args(argc, argv);
-    
-    if (is_empty_args(data))
-        return input_error_message();
-    
-    return simulation(data);
+	t_args	data;
+
+	data = convert_args(argc, argv);
+
+	if (is_empty_args(data))
+		return (input_error_message());
+
+    return (simulation(data));
+
 }
